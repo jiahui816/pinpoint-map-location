12de05669182b4ffbf023c0e18706c9121256f08
Jia Hui Liou <30471836+jiahui816@users.noreply.github.com>
2021-02-17 16:28:56 +1100

    added leaftlet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function App() {
+  const centers = { lat: -37.840935, lng: 144.946457 };
   return (
     <div className="App">
-      <h1>Leaflet App</h1>
+      <MapContainer center={centers} zoom={9} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={{ lat: -37.840935, lng: 144.946457 }}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
